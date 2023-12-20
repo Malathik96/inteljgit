29a11d2a89fae0c343ec8ed8b479ac71c7c9e6bf
malathi@96 <malathikasinathan96@gmail.com>
2023-12-20 15:24:40 +0530

    updated

--- src/test/java/org/example/git_demo.java
+++ src/test/java/org/example/git_demo.java
@@ -3,10 +3,28 @@ package org.example;
 import java.sql.SQLOutput;
 
 public class git_demo {
-public void git(){
-    System.out.println("Hiiiiii");
-}
+
+        public static void main(String[] args) {
+            //datatype ar[]=new datatype;
+
+		/*int a[]=new int[3];
+
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
+
+		System.out.println(a[0]);*/
+
+            System.out.println("demo check");
+
+            int a[]= {100,200,300,400};
+
+            for(int i : a)
+            {
+                System.out.println(i);
+            }
+        }
+
+    }
 
 
-
-}
